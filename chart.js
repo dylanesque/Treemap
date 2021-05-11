210724d96f986efbaabec343d34671455aa2ab57
Michael Caveney <dylanesque27@gmail.com>
2021-05-11 15:54:53 -0400

    Added hierarchical function

--- chart.js
+++ chart.js
@@ -1,7 +1,59 @@
+// example repo: https://www.d3-graph-gallery.com/graph/treemap_json.html
+
 async function drawTree() {
-    const kickStarterDataset = await d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json');
-    const movieDataset = await d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json');
-    const videoGameDataset = await d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json');
+  // 1. Access data
+  const movieDataset = await d3.json(
+    'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+  );
+
+  const movieHierarchy = d3
+    .hierarchy(movieDataset, (node) => {
+      return node.children;
+    })
+    .sum((node) => {
+      return node.value;
+    })
+    .sort((node1, node2) => {
+      return node2.value - node1.value;
+    });
+
+  const movies = movieHierarchy.leaves();
+
+  // 2. Create chart dimensions
+
+  const width = window.innerWidth * 0.85;
+  let dimensions = {
+    width,
+    height: width * 0.4,
+    margin: {
+      top: 5,
+      right: 10,
+      bottom: 50,
+      left: 60,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // 3. Draw canvas
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  const bounds = wrapper
+    .append('g')
+    .style(
+      'transform',
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  const tree = bounds.selectAll('g').data(movies).enter().append('g');
 }
 
 drawTree();
